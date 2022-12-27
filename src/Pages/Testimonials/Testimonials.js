098012109d0afb7abad87e75d4bb6af0c79d9c7a
nurulcse7 <nurul.cse7@gmail.com>
2022-12-27 12:57:29 +0600

    testimonial

--- src/Pages/Testimonials/Testimonials.js
+++ src/Pages/Testimonials/Testimonials.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Testimonials.css'
-import Avatar1 from '../../assets/avatar1.jpg'
-import Avatar2 from '../../assets/avatar2.jpg'
-import Avatar3 from '../../assets/avatar3.jpg'
-import Avatar4 from '../../assets/avatar4.jpg'
+import Avatar1 from '../../assets/testimonial/avatar1.jpg'
+import Avatar2 from '../../assets/testimonial/avatar2.jpg'
+import Avatar3 from '../../assets/testimonial/avatar3.jpg'
+import Avatar4 from '../../assets/testimonial/avatar4.jpg'
 
 // import Swiper core and required modules
 import {Navigation,  Pagination } from 'swiper';
